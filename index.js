2353933cce756fc3c58f127f422f330bf34843e7
Mark Elliott <markelliottva@gmail.com>
2022-05-11 14:38:38 -0400

    added Add functions

--- index.js
+++ index.js
@@ -102,6 +102,167 @@ function displayViewMenu() {
     });
 };
 
+function displayAddMenu() {
+    inquirer
+    .prompt(
+        {
+            type: "list",
+            name: "add_menu",
+            message: "Please select desired Add operation:",
+            choices: ["Department", "Role", "Employee"]
+        }
+    )
+    .then(({add_menu}) => {
+        switch (add_menu) {
+            case "Department":
+                inquirer
+                .prompt (
+                    {
+                        type: "input",
+                        name:   "department",
+                        message: "Please enter the department name:",
+                        validation: nameInput => {
+                            if (nameInput) {
+                                return true;
+                            } else {
+                                console.log('Please enter a name for your Manager!');
+                                return false;
+                            }
+                        }
+                    }
+                )
+                .then((inputDataArray) => {
+                    addDepartment(inputDataArray.department)             
+                    .then((istrue) => {
+                        if (istrue) {
+                        displayInitialMenu();
+                        }
+                     });
+                 });
+                break;
+            case "Role":
+                inquirer
+                .prompt ([
+                    {
+                        type: "input",
+                        name:   "title",
+                        message: "Please enter the role title:",
+                        validation: titleInput => {
+                            if (titleInput) {
+                                return true;
+                            } else {
+                                console.log('Please enter a title for the role!');
+                                return false;
+                            }
+                        }
+                    },
+                    {
+                        type: "number",
+                        name:   "salary",
+                        message: "Please enter the role salary:",
+                        validation: salaryInput => {
+                            if (Number.isInteger(salaryInput)) {
+                                return true;
+                            } else {
+                                console.log('Please enter an integer for the salary for the role!');
+                                return false;
+                            }
+                        }
+                    },
+                    {
+                        type: "number",
+                        name:   "department_id",
+                        message: "Please enter department id:",
+                        validation: idInput => {
+                            if (Number.isInteger(idInput)) {
+                                return true;
+                            } else {
+                                console.log('Please enter an integer for the department id!');
+                                return false;
+                            }
+                        }
+                    }
+                ])
+                .then((inputDataArray) => {
+                    console.log(inputDataArray.title, inputDataArray.salary, inputDataArray.department_id);
+                    addRole(inputDataArray.title, inputDataArray.salary, inputDataArray.department_id)
+                        .then((istrue) => {
+                            if (istrue) {
+                                displayInitialMenu();
+                            }
+                        });
+                });
+                break;
+            case "Employee":
+                inquirer
+                .prompt ([
+                    {
+                        type: "input",
+                        name:   "first_name",
+                        message: "Please enter the employees first name:",
+                        validation: first_nameInput => {
+                            if (first_nameInput) {
+                                return true;
+                            } else {
+                                console.log('Please enter a first name for the employee!');
+                                return false;
+                            }
+                        }
+                    },
+                    {
+                        type: "input",
+                        name:   "last_name",
+                        message: "Please enter the employees last name:",
+                        validation: last_nameInput => {
+                            if (last_nameInput) {
+                                return true;
+                            } else {
+                                console.log('Please enter a last name for the employee!');
+                                return false;
+                            }
+                        }
+                    },
+                    {
+                        type: "number",
+                        name:   "role_id",
+                        message: "Please enter the role id:",
+                        validation: role_idInput => {
+                            if (Number.isInteger(role_idInput)) {
+                                return true;
+                            } else {
+                                console.log('Please enter an integer for the role id!');
+                                return false;
+                            }
+                        }
+                    },
+                    {
+                        type: "number",
+                        name:   "manager_id",
+                        message: "Please enter the manager id:",
+                        validation: manager_idInput => {
+                            if (Number.isInteger(manager_idInput)) {
+                                return true;
+                            } else {
+                                console.log('Please enter an integer for the manager id!');
+                                return false;
+                            }
+                        }
+                    }
+                ])
+            .then((inputDataArray) => {
+                    addAnEmployee(inputDataArray.first_name, inputDataArray.last_name, inputDataArray.role_id, inputDataArray.manager_id)
+                        .then((istrue)=> {
+                            if(istrue) {
+                            displayInitialMenu();   
+                            }
+                        });
+            });
+                break;  
+        }
+    });
+
+};
+
 async function displayEmployeesByManager() {
     const managersArray = await queryAllManagers();
 
@@ -180,15 +341,15 @@ async function displaySalariesByDepartment() {
 
 
 
-// addAnEmployee(first_name, last_name, role_id, manager_id);
+
 // updateEmployeeRole(id,role);
 // updateEmployeeManager(id, manager_id);
 // deleteAnEmployee(id);
 
-// addDepartment(name);
+
 // deleteDepartment(id);
 
-// addRole(title, salary, department_id);
+
 // deleteRole(id);
 
 
